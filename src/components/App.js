23fa3b8fb8448493919858591587409d33dc5806
Ghimirevivek <stephengmre@gmail.com>
2023-04-06 18:03:38 +0530

    message

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,78 @@
-import React from 'react'
-import '../styles/App.css';
-import { Loader } from './Loader';
-import { Todo } from './Todo';
+import React, { useState, useEffect } from 'react'
+import '../styles/App.css'
+import { Loader } from './Loader'
+import { Todo } from './Todo'
 const App = () => {
-  
-  
+  const [todos, setTodos] = useState([])
+  const [loader, setLoader] = useState(false)
+  const [completed, setCompleted] = useState(true)
+  const [incomplete, setIncomplete] = useState(true)
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((data) => {
+        setTodos(data.slice(0, 20))
+        setLoader(false)
+      })
+      .catch((err) => console.log('error'))
+  }, [])
+  const handleCompleteChange = (e) => {
+    setCompleted(e.target.checked)
+  }
+  const handleInompleteChange = (e) => {
+    setIncomplete(e.target.checked)
+  }
+  const filteredTodos = todos.filter((todo) => {
+    if (completed && incomplete) {
+      return true
+    } else if (completed && todo.completed) {
+      return true
+    } else if (incomplete && !todo.completed) {
+      return true
+    } else {
+      return false
+    }
+  })
+  return (
+    <>
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <ol>
+            {filteredTodos.map((todo) => (
+              <Todo
+                key={todo.id}
+                id={todo.id}
+                title={todo.title}
+                completed={todo.completed}
+              />
+            ))}
+          </ol>
+          <div id="filter-holder">
+            <label>
+              <input
+                type="checkbox"
+                id="completed-checkbox"
+                checked={completed}
+                onChange={handleCompleteChange}
+              />
+              Show Completed
+            </label>
+            <label>
+              <input
+                type="checkbox"
+                id="incompleted-checkbox"
+                checked={incomplete}
+                onChange={handleInompleteChange}
+              />
+              Show Inompleted
+            </label>
+          </div>
+        </>
+      )}
+    </>
+  )
 }
 
-
-export default App;
+export default App
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,14 @@
- import React from 'react';
+import React from 'react'
 
- export const Todo  = () =>{
-    
- }
+export const Todo = ({ id, title, completed }) => {
+  return (
+    <>
+      <li className="todo" id={`todo-${id}`}>
+        <div className="todo-text">{title}</div>
+        <div className="todo-status">
+          {completed ? 'Complete' : 'Incomplete'}
+        </div>
+      </li>
+    </>
+  )
+}
